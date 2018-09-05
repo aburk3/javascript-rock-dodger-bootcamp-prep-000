a1719bbc73c50a8efb9fb29c45a8731bd5102b2b
Austin  Burke <aburk3@gmail.com>
2018-09-05 15:51:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -147,7 +147,14 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
-  window.requestAnimationFrame
+  window.requestAnimationFrame(function(){
+    const left = positionToInteger(DODGER.style.left) 
+    
+    if (left > 0) {
+      DODGER.style.left = `$(left - 4)px`
+    }
+  })
+    
 }
 
 function moveDodgerRight() {
@@ -156,6 +163,13 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   window.requestAnimationFrame(function(){
+    const left = positionToInteger(DODGER.style.left) 
+    
+    if (left > 0) {
+      DODGER.style.left = `$(left - 4)px`
+    }
+  })
 }
 
 /**
