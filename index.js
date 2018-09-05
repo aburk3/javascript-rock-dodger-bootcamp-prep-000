b028f8973b2fb83df992dc502226e3263b557cb9
Austin  Burke <aburk3@gmail.com>
2018-09-05 15:36:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -135,16 +135,19 @@ function moveDodger(e) {
     const code = e.which
     
     if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
-      
+      e.preventDefault()
+      e.stopPropagation()
+    }
+    
+    if (code === LEFT_ARROW) {
+      moveDodgerLeft()
+    } else if (code === RIGHT_ARROW) {
+      moveDodgerRight()
     }
 }
 
 function moveDodgerLeft() {
-  // implement me!
-  /**
-   * This function should move DODGER to the left
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+  window.requestAnimationFrame
 }
 
 function moveDodgerRight() {
